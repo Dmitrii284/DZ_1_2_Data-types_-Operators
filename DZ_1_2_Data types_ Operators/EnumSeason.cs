4322a481453b8909978d28774b3ec9d58432e69a
Дмитрий <@ADMIN>
2023-10-06 15:31:15 +0500

    1

---
+++ DZ_1_2_Data types_ Operators/EnumSeason.cs
@@ -0,0 +1,40 @@
+﻿/*
+ * Задача 5
+ Пользователь вводит с клавиатуры дату. Приложение должно отобразить название сезона и дня недели. 
+Например, если введено 22.12.2021, приложение должно 
+отобразить Winter Wednesday
+
+ */
+
+namespace DZ_1_2_Data_types__Operators
+{
+    internal partial class Program
+    {
+        public enum EnumSeason
+        {
+           1 =  "Winter",
+            Spring,
+            Summer,
+            Autumn
+        } 
+
+        
+    }
+}
+//public interface Dictionary<TKey, TValue>
+//{
+//   public TKey key { get; set; }
+//   public TValue this[TKey key] { get; set; }
+//}
+//public class SeasonMap< TKey, TValue> {
+//    private readonly System.Collections.Generic.Dictionary<TKey, TValue> _map
+//        = new System.Collections.Generic.Dictionary<TKey, TValue>();
+//    public TValue this[TKey key]
+//    {
+//        get { return _map[key]; }
+//        set { _map[key] = value; }
+//    }
+//    public void Add(TKey tkey, TValue value) { _map.Add(tkey, value); }
+//    public bool ContainsKey(TKey key) { return _map.ContainsKey(key); }
+//}
+
--- DZ_1_2_Data types_ Operators/Program.cs
+++ DZ_1_2_Data types_ Operators/Program.cs
@@ -6,61 +6,132 @@
 
  */
 
-using System.Collections;
+//using global::System.Globalization;
+
+//namespace DZ_1_2_Data_types__Operators
+//{
+//    internal partial class Program
+//    {
+        //public enum EnumSeason
+        //{
+        //    Winter,
+        //    Spring,
+        //    Summer,
+        //    Autumn
+        //}
+
+        //public static int Main(string[] args)
+        //{
+        //    static string GetSeason(DateTime date)
+        //    {
+        //        string season = "";
+        //        int month = DateTime.Now.Month;
+        //        public enum Season
+        //{ Winter = 1,
+        //    Spring = 3,
+        //            ,
+        //    1, 2; }
+        //Winter = 12, 1, 2;
+                  
+                // switch(month)
+                //{
+                //    case 12:
+                //    case 1: 
+                //    case 2:  season = "Winter"; break;
+                //    case 3:
+                //    case 4:
+                //    case 5: season = "Spring"; break;
+                //    case 6:
+                //    case 7:
+                //    case 8: season = "Summer"; break;
+                //    case 9:
+                //    case 10:
+                //    case 11: season = "Autumn"; break;
+                //    default: throw new ArgumentException("Invalid date");                
+                
+//            }
+
+//             Console.WriteLine("Task 5\nFind out what time of year and day of the week!!\nEnter the datr with format 20.10.2023 -> ");
+//             string? input = Console.ReadLine();
+
+//            if(DateTime.TryParseExact(input,"dd.MM.yyyy", CultureInfo.InvariantCulture,DateTimeStyles.None, out DateTime date))
+//            {
+//                string season = date.ToString("MM", CultureInfo.InvariantCulture);
+//                string dayOfWeek = date.ToString("dddd", CultureInfo.InvariantCulture);
+//                Console.WriteLine($"Resault:{season}, {dayOfWeek}");
+//            }
+//            Console.WriteLine("Mistake!Invalid date format!");
+
+//            return 0;
+//        }
+//    }
+//}
+
+using System;
 using System.Globalization;
-using System.Collections.Generic;
-using System.Data;
 
-
-
-namespace DZ_1_2_Data_types__Operators
+namespace SeasonAndDayOfWeek
 {
-    internal class Program
+    class Program
     {
+        enum Season
+        {
+            Winter,
+            Spring,
+            Summer,
+            Autumn
+        };
+        
+
         static void Main(string[] args)
         {
-            //Task 5
-            Console.WriteLine("Task 5\nFind out what time of year and day of the week!!\nEnter the datr with format 20.10.2023 -> ");
-            string input = Console.ReadLine();
-
-            DateTime date;
-            if (DateTime.TryParseExact(input,"dd.MM.YYYY", CultureInfo.InvariantCulture, DateTimeStyles.None, out date)) {
-                string season = GetSeason(date);
-                string dayOfWeek = date.ToString("dddd", CultureInfo.InvariantCulture);
-                Console.WriteLine($"Result: {season}{dayOfWeek}");
+            if (args is null)
+            {
+                throw new ArgumentNullException(nameof(args));
             }
-            Console.WriteLine("Mistake!Invalid date format!");
+
+            Console.WriteLine("Задача 5\nПользователь вводит дату. Приложение должно отобразить название сезона и дня недели.");
+            Console.Write("Введите дату (dd.MM.yyyy): ");
+            string? input = Console.ReadLine();
+
+            if (DateTime.TryParseExact(input, "dd.MM.yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime date))
+            {
+                Season season = GetSeason(date);
+                string dayOfWeek = date.ToString("dddd", CultureInfo.InvariantCulture);
+
+                Console.WriteLine($"Название сезона: {season}");
+                Console.WriteLine($"День недели: {dayOfWeek}");
+            }
+            else
+            {
+                Console.WriteLine("Ошибка! Некорректный формат даты.");
+            }
         }
 
-      static string GetSeason(DateTime date)  {
-
-            Dictionary<int, string> season = new Dictionary<int, string>
-            (
-                { 1, "Winter" },
-                { 2, "Spring" },
-                { 3, "Summer" },
-                { 4, "Autumn" }
-            );
+        static Season GetSeason(DateTime date)
+        {
             int month = date.Month;
-            int seasonNumber = (month + 3) % 3;
-            return season[seasonNumber];
-        }        
+
+            if (month == 12 || month == 1 || month == 2)
+            {
+                return Season.Winter;
+            }
+            else if (month >= 3 && month <= 5)
+            {
+                return Season.Spring;
+            }
+            else if (month >= 6 && month <= 8)
+            {
+                return Season.Summer;
+            }
+            else
+            {
+                return Season.Autumn;
+            }
+        }
     }
 }
-public interface Dictionary<TKey, TValue>
-{
-   public TKey key { get; set; }
-   public TValue this[TKey key] { get; set; }
-}
-public class SeasonMap< TKey, TValue> {
-    private readonly System.Collections.Generic.Dictionary<TKey, TValue> _map
-        = new System.Collections.Generic.Dictionary<TKey, TValue>();
-    public TValue this[TKey key]
-    {
-        get { return _map[key]; }
-        set { _map[key] = value; }
-    }
-    public void Add(TKey tkey, TValue value) { _map.Add(tkey, value); }
-    public bool ContainsKey(TKey key) { return _map.ContainsKey(key); }
-}
+
+
+
 
